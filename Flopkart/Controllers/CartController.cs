e6d0ec8050ffe7cf76df19bc731397aa9d431eb8
insane-03 <118243063+insane-03@users.noreply.github.com>
2022-11-29 12:47:13 +0530

    CRUD Operation - Addion(Cart Controller)

---
+++ Flopkart/Controllers/CartController.cs
@@ -0,0 +1,66 @@
+﻿
+
+using Flopkart_Model;
+using Flopkart_Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Flopkart.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartService _cartService;
+
+        private readonly ILogger<CartController> _logger;
+        public CartController(ICartService cartService, ILogger<CartController> logger)
+        {
+            
+            _cartService = cartService;
+            _logger = logger;
+        }
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return Ok("HEY I AM CART");
+        }
+        //[HttpPut("BuyProduct/{ProductId}/{Quantity}")]
+
+        //public IActionResult BuyProduct(int ProductId,string ProductName,int Quantity)
+        //{
+        //    try
+        //    {
+        //        if(ProductId != 0 && ProductName != null && Quantity != 0)
+        //        {
+        //            return Ok(_cartService.SelectProduct(ProductId,ProductName,Quantity));
+        //        }
+        //        return NotFound();
+
+        //    }catch(Exception ex)
+        //    {
+        //        _logger.LogInformation(ex.Message);
+        //        throw;
+        //    }
+
+        [HttpPut("BuyProduct")]
+
+        public IActionResult BuyProduct([FromBody] ProductModel product)
+        {
+            try
+            {
+                if (product != null)
+                {
+                    return Ok(_cartService.SelectProduct(product));
+                }
+                return NotFound();
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation(e.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ Flopkart/Startup.cs
@@ -0,0 +1,74 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+using Flopkart_Service.Implementation;
+
+using Flopkart_Service.Interface;
+
+using Flopkart_Repository.Implementation;
+
+using Flopkart_Repository.Interface;
+
+using Flopkart_Repository;
+
+
+namespace Flopkart
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen();
+
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+            services.AddTransient<ICartService, CartService>();
+            services.AddTransient<ICartRepository, CartRepository>();
+
+            
+            
+
+            services.AddSingleton<DB_Context>(
+                k => new DB_Context(Configuration.GetConnectionString("MongoDB"),
+                Configuration.GetValue<string>("DatabaseName")));
+
+
+            services.AddAutoMapper(typeof(IProductService));
+            services.AddAutoMapper(typeof(ICartService));
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment()) 
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flopkart");
+            });
+
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapControllers();
+            });
+             
+        }
+    }
+}
